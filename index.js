415f00afc659424585b34aeff7dcccfd3927a2bf
avi <avirathtibrewala@gmail.com>
2021-06-28 08:04:37 -0400

    Seconf commit

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const {WebhookClient} = require('dialogflow-fulfillment')
 const app = express()
 app.use(bodyParser.json())
 
-const port = process.env.PORT || 3333
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  console.log("Server started working")
+  res.send("Server started working")
+})
 
 app.post('/dialogflow-fulfillment', (req, res) => {
   dialogflowFulfillment(req, res)
@@ -13,7 +18,9 @@ app.post('/dialogflow-fulfillment', (req, res) => {
 app.listen(port, () => console.log(`listening on port ${port}`));
 
 const dialogflowFulfillment = (req, res) => {
-    const agent = new WebhookClient({req, res})
+    console.log("Webhook client Response")
+    console.log(req)
+    const agent = new WebhookClient({request:req, response:res})
 
     function sayHello(agent) {
         agent.add("Hi there. This response is coming from the Server")
